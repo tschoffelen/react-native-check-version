e82aa382aae7138585efffd05b841db79ff6566f
Thomas Schoffelen <thomas@schof.co>
2020-04-04 17:33:05 +0100

    Cleanup.

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,21 @@
-import { PlatformOSType } from 'react-native';
+import {PlatformOSType} from 'react-native';
 
 export type CheckVersionUpdateType = 'major' | 'minor' | 'patch';
 
 export interface CheckVersionOptions {
-	endpoint?: string;
-	platform?: PlatformOSType;
-	bundleId?: string;
-	currentVersion?: string;
+    endpoint?: string;
+    platform?: PlatformOSType;
+    bundleId?: string;
+    currentVersion?: string;
 }
 
 export interface CheckVersionResponse {
-	version: string;
-	released?: Date; // iOS only
-	url: string;
-	notes?: string; // iOS only
-	needsUpdate: boolean;
-	updateType: CheckVersionUpdateType;
+    version: string;
+    released?: Date; // iOS only
+    url: string;
+    notes?: string; // iOS only
+    needsUpdate: boolean;
+    updateType: CheckVersionUpdateType;
 }
 
 declare const checkVersion: (options?: CheckVersionOptions) => Promise<CheckVersionResponse>;
