5d305a38064e7305eb372ffa61815822f3d839ef
Thomas Schoffelen <thomas@schof.co>
2021-03-29 12:26:32 +0100

    Fix tests.

--- tests/index.test.js
+++ tests/index.test.js
@@ -10,12 +10,24 @@ describe('checkVersion', () => {
   test('can get version for valid bundle ID', async() => {
     const data = await checkVersion({
       platform: 'android',
+      bundleId: 'com.streetartcities.map',
+      currentVersion: '1.0.0'
+    });
+
+    expect(data.platform).toEqual('android');
+    expect(data.bundleId).toEqual('com.streetartcities.map');
+    expect(data.needsUpdate).toEqual(true);
+  });
+
+  test('will not update when version is "Varies with device"', async() => {
+    const data = await checkVersion({
+      platform: 'android',
       bundleId: 'com.facebook.katana',
       currentVersion: '1.0.0'
     });
 
     expect(data.platform).toEqual('android');
     expect(data.bundleId).toEqual('com.facebook.katana');
-    expect(data.needsUpdate).toEqual(true);
+    expect(data.needsUpdate).toEqual(false);
   });
 });
