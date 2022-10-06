545d21f47952694524c5d18c2211a23803615c6b
Dawid Zawadzki <zawadzki@devanddeliver.com>
2022-10-06 09:27:47 +0200

    fix: update android provider headers

--- src/providers/android.js
+++ src/providers/android.js
@@ -6,7 +6,8 @@ export const getAndroidVersion = async(bundleId, country) => {
   try {
     res = await axios.get(url, {
       headers: {
-        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36",
+        'sec-fetch-site': 'same-origin'
       }
     });
   } catch (e) {
