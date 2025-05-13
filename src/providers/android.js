ceac90692d92b3aaa655ad77f541a70de21f4c03
Carlos Ferreira <carlossts826@gmail.com>
2025-05-13 20:35:33 -0300

    feat: Get release at from playStore

--- src/providers/android.js
+++ src/providers/android.js
@@ -25,10 +25,11 @@ export const getAndroidVersion = async (bundleId, country) => {
   const text = await res.text();
   const version = text.match(/\[\[\[['"]((\d+\.)+\d+)['"]\]\],/)[1];
   const notes = text.match(/<div itemprop="description">(.*?)<\/div>/)?.[1];
+  const releasedAt = text.match(/<div class="xg1aie">(.*?)<\/div>/)?.[1];
 
   return {
     version: version || null,
-    releasedAt: (new Date()).toISOString(),
+    releasedAt: releasedAt || "",
     notes: notes || "",
     url: `https://play.google.com/store/apps/details?id=${bundleId}&hl=${country}`,
     lastChecked: (new Date()).toISOString()
